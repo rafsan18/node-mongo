ca72bb54150db0d8b8382f27639ea6d887f1aa14
rafsan <rafsanamin18@gmail.com>
2020-09-30 19:42:07 +0600

    47.5

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express = require("express");
 
 const app = express();
 
+const users = ["Asad", "Moin", "Saber", "Susmita", "Sohana", "Sabana"];
+
 app.get("/", (req, res) => {
-    res.send("I know how to open node ");
+    const fruit = {
+        product: "ada",
+        price: 220,
+    };
+    res.send(fruit);
+});
+
+app.get("/fruits/banana", (req, res) => {
+    res.send({ fruit: "banana", quantity: 1000, price: 10000 });
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(req.query);
+    const name = users[id];
+    res.send({ id, name });
 });
 
 app.listen(3000, () => console.log("Listening to port 3000"));
