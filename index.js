720b6be4a327e855660db7bfb704170c3e09e1f2
rafsan <rafsanamin18@gmail.com>
2020-09-30 22:01:22 +0600

    end of 47

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 const users = ["Asad", "Moin", "Saber", "Susmita", "Sohana", "Sabana"];
 
@@ -12,15 +17,23 @@ app.get("/", (req, res) => {
     res.send(fruit);
 });
 
-app.get("/fruits/banana", (req, res) => {
-    res.send({ fruit: "banana", quantity: 1000, price: 10000 });
-});
+// app.get("/fruits/banana", (req, res) => {
+//     res.send({ fruit: "banana", quantity: 1000, price: 10000 });
+// });
 
 app.get("/users/:id", (req, res) => {
     const id = req.params.id;
-    console.log(req.query);
     const name = users[id];
     res.send({ id, name });
+});
+
+// post
+
+app.post("/addUser", (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user);
 });
 
 app.listen(3000, () => console.log("Listening to port 3000"));
